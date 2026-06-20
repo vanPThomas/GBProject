787fb3bfe2afcf650b681b1530caa511d6dabfa3
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-06-20 06:26:52 +0200

    Placed sprites in seperate file

---
+++ sprites.h
@@ -0,0 +1,10 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+extern unsigned char PlayerSpriteLeftFacing[];
+extern unsigned char PlayerSpriteRightFacing[];
+extern unsigned char BulletSprite[];
+extern unsigned char WallTile[];
+extern unsigned char FloorTile[];
+
+#endif
