1e3b8574b847b0be004c775fbad576a5b10b770d
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-06-21 02:15:42 +0200

    added custom letter sprites

--- sprites.h
+++ sprites.h
@@ -7,4 +7,11 @@ extern unsigned char BulletSprite[];
 extern unsigned char WallTile[];
 extern unsigned char FloorTile[];
 
+extern unsigned char S[];
+extern unsigned char C[];
+extern unsigned char O[];
+extern unsigned char R[];
+extern unsigned char E[];
+extern unsigned char :[];
+
 #endif
