a18e27cbf46a0c3b0df8b1b022dfef21cff97dc8
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-06-24 12:20:01 +0200

    Created number sprites

--- sprites.h
+++ sprites.h
@@ -18,4 +18,16 @@ extern unsigned char L_char[];
 extern unsigned char I_char[];
 extern unsigned char V_char[];
 
+extern unsigned char char0[];
+extern unsigned char char1[];
+extern unsigned char char2[];
+extern unsigned char char3[];
+extern unsigned char char4[];
+extern unsigned char char5[];
+extern unsigned char char6[];
+extern unsigned char char7[];
+extern unsigned char char8[];
+extern unsigned char char9[];
+
+
 #endif
