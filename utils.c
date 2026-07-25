9b83c2e9bc218afa4e042f94c4484d12409a6133
Thomas Van Petegem <thomas.van.petegem@hotmail.com>
2026-07-25 10:17:45 +0200

    Added collision between coordinates

--- utils.c
+++ utils.c
@@ -8,3 +8,18 @@
 //     int value = rand() % (max - min + 1) + min;
 //     return (uint8_t) value;
 // }
+
+// Returns 1 if the two sprites are overlapping, 0 otherwise
+uint8_t check_collision(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
+{
+    // Sprites are 8x8
+    // We check if the rectangles overlap
+    if (x1 < x2 + 8 &&
+        x1 + 8 > x2 &&
+        y1 < y2 + 8 &&
+        y1 + 8 > y2)
+    {
+        return 1;   // Collision!
+    }
+    return 0;       // No collision
+}
--- utils.h
+++ utils.h
@@ -3,4 +3,7 @@
 
 // uint8_t random_between(uint8_t min, uint8_t max);
 
+uint8_t check_collision(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2)
+
+
 #endif
